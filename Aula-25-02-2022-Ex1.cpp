1641e470dd5dfbc813c18870b924147dc2937f12
Nicolas Fernandes Ferrari <79235449+NicolasNFF@users.noreply.github.com>
2022-02-25 09:06:05 -0300

    Create Aula-25-02-2022-Ex1.cpp

---
+++ Aula-25-02-2022-Ex1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define
+
+int mian(void)
+{
+    int vetor[VECTOR_SIZE];
+    int indx;
+
+    for(indx=0; index < VECTOR_SIZE; indx++)
+    {
+        
+    }
+}
